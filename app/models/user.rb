b255d753e45000187c79c87128c45dbb6343483e
Margus Ernits <margus.ernits@itcollege.ee>
2010-09-16 12:26:29 +0300

    lisatud devise ldap autentimine

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  devise :ldap_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+end
