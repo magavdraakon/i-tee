c9ba181952c79139d61e40f930357d95acee8f9d
unknown <mad@.(none)>
2010-10-03 18:26:55 +0300

    Changed fake config property to emulate. Added a new layout for login prcedures. Modified application layout to suit the new design needs. Added new ui files to git. Prototyped new design using host view.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,49 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+function showDialog(title, msg, type) {
+	$("#dialog").empty();
+
+	$("#dialog").append("<p>" + msg + "</p>");
+
+
+	$("#dialog").dialog({
+		modal: true,
+		title: title,
+		dialogClass: type,
+		buttons: {
+			"Close": function() {
+				$(this).dialog("close");
+			}
+		}
+	});
+}
+
+$(document).ready(function(){
+	$(".round").corner("5px");
+	$(".round-top").corner("top 5px");
+
+	jQuery(function ($) {
+		$('#basic-modal .modal').click(function (e) {
+			$('#basic-modal-content').modal();
+			return false;
+		});
+	});
+
+
+	$("#menu-hide").click(function() {
+		$('#left').hide("slow", function(){
+			$('#right').css("margin-left", 0);
+			$('#menu-show').css("display", "block");
+		});		
+	});
+
+	$("#menu-show").click(function() {
+		$('#right').css("margin-left", 300);
+		$('#left').show("slow");
+		$('#menu-show').css("display", "none");
+	});
+
+
+});
