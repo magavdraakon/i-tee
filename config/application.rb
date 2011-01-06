6c32ee376323a4440a5885b5373f74e67d148dd3
tiiatanav <tiiatanav@hotmail.com>
2011-01-06 16:55:01 +0200

    updated the gemfile and readme

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module ITee
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
+    config.autoload_paths += %W( #{config.root}/app/models/ckeditor )
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
