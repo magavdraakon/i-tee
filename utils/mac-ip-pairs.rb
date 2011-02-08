d8a7a8dfb6462c14e4211319fb1129d34266aa48
tiiatanav <tiiatanav@hotmail.com>
2011-02-08 11:40:08 +0200

    added a ruby script that generates mac-ip pairs for the DHCP server

---
+++ utils/mac-ip-pairs.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+for i in 102..220
+puts "host virtlab#{i} {"
+puts "hardware ethernet 52:54:00:e9:8b:#{i.to_s(16)};"
+puts "fixed-address 192.168.13.#{i};"
+puts "}"
+
+end
+puts "}"
