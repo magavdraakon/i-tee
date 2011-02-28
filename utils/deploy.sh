8e761307689ef05171cad4d38469831db807db83
tiiatanav <tiiatanav@hotmail.com>
2011-02-28 16:49:21 +0200

    production deploy script update

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -3,7 +3,9 @@ cd /var/www/railsapps/i-tee
 git pull
 if [ $? -eq 0 ]
   then
+  echo "migrating database"
   rake db:migrate RAILS_ENV='production'
+  echo "restarting rails app"
   touch /var/www/railsapps/i-tee/tmp/restart.txt
 else
   echo "Git error, try updating manually"
