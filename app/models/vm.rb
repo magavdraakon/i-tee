69742a476ea88a9a466d49352f5696d2246d9729
tiiatanav <tiiatanav@hotmail.com>
2011-03-15 13:23:36 +0200

    vm: fixed a naming error in model

--- app/models/vm.rb
+++ app/models/vm.rb
@@ -2,10 +2,11 @@ class Vm < ActiveRecord::Base
   has_one :mac
   belongs_to :user
   belongs_to :lab_vmt
-  before_destroy :delete_vms
+  before_destroy :del_vm
 
   def del_vm
     return %x(/var/www/railsapps/i-tee/utils/stop_machine.sh #{name}  2>&1)
   end
+  
   
 end
