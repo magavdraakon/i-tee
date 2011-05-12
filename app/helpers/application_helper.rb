67c081509a93cf4d9f0f172c2d7aede070dfec11
tiiatanav <tiiatanav@hotmail.com>
2011-05-12 18:12:48 +0300

    lab: added materials list to notes, removed invalid material id error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,9 @@ module ApplicationHelper
   
   def link_M(string)
   string.gsub(/#M_(\d+)/) do
-    link_to "#{Material.find_by_id($1).name}", material_path($1), :target => '_blank'
-  end
+  material=Material.find_by_id($1)
+   link_to "#{material.name}", material_path($1), :target => '_blank' if material!=nil
+ end
 end
 
 end
