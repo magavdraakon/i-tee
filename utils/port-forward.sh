b6da4a71c63d1c14222379eb4682ca5cfb273b01
Margus Ernits <margus.ernits@gmail.com>
2011-07-26 18:26:38 +0300

    added br1 interface to iptables script

--- utils/port-forward.sh
+++ utils/port-forward.sh
@@ -4,3 +4,9 @@
 for ip in $(seq 102 220); do
 /sbin/iptables -t nat -A PREROUTING -p tcp -i br0 --dport 8${ip} -j DNAT --to 192.168.13.${ip}:4200
 done
+
+
+for ip in $(seq 102 220); do
+/sbin/iptables -t nat -A PREROUTING -p tcp -i br1 --dport 8${ip} -j DNAT --to 192.168.13.${ip}:4200
+done
+
