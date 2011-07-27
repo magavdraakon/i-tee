29fc7f0ff8b864a652520acefd831cb083910c6a
Margus Ernits <margus.ernits@gmail.com>
2011-07-27 17:57:31 +0300

    utils/port-forward.sh - fixed port forwarding rules

--- utils/port-forward.sh
+++ utils/port-forward.sh
@@ -2,11 +2,11 @@
 
 #Port forwarding script
 for ip in $(seq 102 220); do
-/sbin/iptables -t nat -A PREROUTING -p tcp -i br0 --dport 8${ip} -j DNAT --to 192.168.13.${ip}:4200
+/sbin/iptables -t nat -A PREROUTING -p tcp -i br0 -d 192.168.13.2 --dport 8${ip} -j DNAT --to 192.168.13.${ip}:4200
 done
 
 
 for ip in $(seq 102 220); do
-/sbin/iptables -t nat -A PREROUTING -p tcp -i br1 --dport 8${ip} -j DNAT --to 192.168.13.${ip}:4200
+/sbin/iptables -t nat -A PREROUTING -p tcp -i br1 -d 193.40.194.185 --dport 8${ip} -j DNAT --to 192.168.13.${ip}:4200
 done
 
