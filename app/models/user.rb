ee3b528fb091de716cb2c2dcc8d01cca64929e04
Tiia Tänav <tiiatanav@hotmail.com>
2013-05-31 11:40:39 +0300

    changed badge displaying size, fixed user.has_badge

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ActiveRecord::Base
   end
 
   def has_badge(lab_badge_id)
-    ub=UserBadge.find(:all, :conditions=>["lab_badge_id=? and id=?", lab_badge_id, id])
+    ub=UserBadge.find(:all, :conditions=>["lab_badge_id=? and user_id=?", lab_badge_id, id])
     return ub.count>0
   end
 
