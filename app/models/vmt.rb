0741f71e0809b59e31481778774d982d3f18313f
Tiia Tänav <tiiatanav@hotmail.com>
2014-12-08 11:39:32 +0200

    Vmt.rb: get all available vm template files on server and use it in vmt creation

--- app/models/vmt.rb
+++ app/models/vmt.rb
@@ -2,4 +2,12 @@ class Vmt < ActiveRecord::Base
   has_many :lab_vmts, :dependent => :destroy
   belongs_to :operating_system
   validates_presence_of :image, :username, :operating_system_id
+
+  def available
+    available=[]
+    %x(vboxmanage list vms | grep template|cut -d' ' -f1|tr '"' ' ').split("\n").each do |vmt|
+      available<< vmt.strip
+    end
+    available
+  end
 end
