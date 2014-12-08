df5141a61ff459475458b1de7cb0b8b377010c1b
Margus Ernits <margus.ernits@gmail.com>
2014-12-08 22:07:45 +0200

    remove all.js in deploy of new version

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -4,6 +4,7 @@ git pull
 if [ $? -eq 0 ]; then
   echo "migrating database"
   rake db:migrate RAILS_ENV='production'
+  rm public/javascripts/all.js
   echo "restarting rails app"
   touch /var/www/railsapps/i-tee/tmp/restart.txt
 else
