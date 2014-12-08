e04f15d1a4c81cadb15a14caf3d0c4fe83b2f0b3
Tiia Tänav <tiiatanav@hotmail.com>
2014-12-08 19:02:57 +0200

    disabled vm progress polling, added sudo prefix to the new method in vmt.rb

--- app/models/vmt.rb
+++ app/models/vmt.rb
@@ -3,9 +3,16 @@ class Vmt < ActiveRecord::Base
   belongs_to :operating_system
   validates_presence_of :image, :username, :operating_system_id
 
+  if ITee::Application::config.respond_to? :cmd_perfix
+    @exec_line = ITee::Application::config.cmd_perfix
+  else
+    @exec_line = 'sudo -u vbox '
+  end
+
+
   def available
     available=[]
-    %x(vboxmanage list vms | grep template|cut -d' ' -f1|tr '"' ' ').split("\n").each do |vmt|
+    %x(#{@exec_line} vboxmanage list vms | grep template|cut -d' ' -f1|tr '"' ' ').split("\n").each do |vmt|
       available<< vmt.strip
     end
     available
