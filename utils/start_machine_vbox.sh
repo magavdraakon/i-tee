6d251437639bbdb97f0916d00c4bf3837d10312f
Margus Ernits <margus.ernits@gmail.com>
2015-09-22 09:40:49 -0400

    turn back user deletion on VM start

--- utils/start_machine_vbox.sh
+++ utils/start_machine_vbox.sh
@@ -89,7 +89,7 @@ if [[ -r /var/labs/run/${TEMPLATE}.sh ]]
 then
 source /var/labs/run/${TEMPLATE}.sh
 
-#curl -H 'Content-Type: application/json' -X DELETE -d '{"api_key":"'"${API_KEY_ADMIN}"'", "lab":"'"${LAB_ID}"'", "userName":"'"${USERNAME}"'", "reset":false}' "${LAB_URI}"
+curl -H 'Content-Type: application/json' -X DELETE -d '{"api_key":"'"${API_KEY_ADMIN}"'", "lab":"'"${LAB_ID}"'", "userName":"'"${USERNAME}"'", "reset":false}' "${LAB_URI}"
 
 echo "'Content-Type: application/json' -X POST -d '{"api_key":"'"$API_KEY_ADMIN"'", "lab":"'"$LAB_ID"'", "username":"'"$USERNAME"'", "password":"'"${USER_PWD}"'", "info":{"answer":"42"}}' "${LAB_URI}""
 #curl -H "Content-Type: application/json" -X GET -d '{"api_key":"botkey", "username":"someone"}' http://localhost:3000/api/v1/userkey
