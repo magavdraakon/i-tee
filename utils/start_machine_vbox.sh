69d430af17991406d22a0ca9b69d683622c9dae4
Margus Ernits <margus.ernits@gmail.com>
2016-01-05 17:10:48 +0200

    added host to create lab user in virtualTA

--- utils/start_machine_vbox.sh
+++ utils/start_machine_vbox.sh
@@ -25,7 +25,7 @@ else
     exit 1
 fi
 
-IT_HOST=$1
+IT_HOSTNAME=$1
 IP_ADDR=$2
 TEMPLATE=$3
 NAME=$4
@@ -87,10 +87,7 @@ source /var/labs/run/${TEMPLATE}.sh
 
 curl -k -H 'Content-Type: application/json' -X DELETE -d '{"api_key":"'"${API_KEY_ADMIN}"'", "lab":"'"${LAB_ID}"'", "userName":"'"${USERNAME}"'", "reset":false}' "${LAB_URI}"
 
-#echo "SENT user delete" 'Content-Type: application/json' -X POST -d '{"api_key":"'"${API_KEY_ADMIN}"'", "lab":"'"${LAB_ID}"'", "username":"'"${USERNAME}"'", "password":"'"${USER_PWD}"'", "info":{"answer":"42"}}' "${LAB_URI}"
-
-#curl -k -H "Content-Type: application/json" -X GET -d '{"api_key":"botkey", "username":"someone"}' http://localhost:3000/api/v1/userkey
-USER_KEY=$(curl -k -H 'Content-Type: application/json' -X POST -d '{"api_key":"'"${API_KEY_ADMIN}"'", "lab":"'"${LAB_ID}"'", "fullname":"'"${FULLNAME}"'", "username":"'"${USERNAME}"'", "password":"'"${USER_PWD}"'", "info":{"answer":"42"}}' "${LAB_URI}" | cut -d'"' -f4 -)
+USER_KEY=$(curl -k -H 'Content-Type: application/json' -X POST -d '{"api_key":"'"${API_KEY_ADMIN}"'", "lab":"'"${LAB_ID}"'", "fullname":"'"${FULLNAME}"'", "username":"'"${USERNAME}"'", "password":"'"${USER_PWD}"'", "host":"'"${IT_HOSTNAME}"'", "info":{"answer":"42"}}' "${LAB_URI}" | cut -d'"' -f4 -)
 
 echo USER_KEY is $USER_KEY
 logger -p info -t i-tee  "USER_KEY for user ${USERNAME} is $USER_KEY for VM  ${TEMPLATE}"
