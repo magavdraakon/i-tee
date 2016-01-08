af15febbac2299a11f71e1f6ce36e2f741475569
Margus Ernits <margus.ernits@rangeforce.com>
2016-01-08 08:46:39 -0600

    temporar fix for lab_user

--- app/models/lab_user.rb
+++ app/models/lab_user.rb
@@ -8,10 +8,10 @@ class LabUser < ActiveRecord::Base
 # get all vms that belong to this labuser (Lab attempt)
   def vms
     #find templates for lab
-  	#vmts=LabVmt.where("lab_id = ? ", self.lab_id)
+  	vmts=LabVmt.where("lab_id = ? ", self.lab_id)
     #find vms for user in lab
-  	#Vm.where("user_id=? and lab_vmt_id in (?)", self.user_id, vmts)
-    Vm.joins(:lab_vmt).where("lab_vmts.lab_id=? and vms.user_id=?", self.lab_id, self.user_id).order('position asc')
+  	Vm.where("user_id=? and lab_vmt_id in (?)", self.user_id, vmts)
+    #Vm.joins(:lab_vmt).where("lab_vmts.lab_id=? and vms.user_id=?", self.lab_id, self.user_id).order('position asc')
   end
 
   def vm_statistic
