82944b0f7d7cb77f68cc591d77f5b045801d7cd2
tiiatanav <tiiatanav@hotmail.com>
2016-03-28 16:42:14 +0300

    Update 20160307154117_add_timeouts_to_lab.rb
    new labs have delayed jobs turned off by default

--- db/migrate/20160307154117_add_timeouts_to_lab.rb
+++ db/migrate/20160307154117_add_timeouts_to_lab.rb
@@ -1,7 +1,7 @@
 class AddTimeoutsToLab < ActiveRecord::Migration
   def change
-    add_column :labs, :poll_freq, :integer, :default => 10*60 # 10 minutes
-    add_column :labs, :end_timeout, :integer, :default => 0 # no timeout
-    add_column :labs, :power_timeout, :integer, :default => 45*60 # 45 minutes
+    add_column :labs, :poll_freq, :integer, :default => 0 # disabled
+    add_column :labs, :end_timeout, :integer, :default => 0 # disabled
+    add_column :labs, :power_timeout, :integer, :default => 0 # disabled
   end
 end
