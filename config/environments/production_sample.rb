3337f0714770cfe38fc4b1e6ea9b0de526f58cc2
keijokapp <keijo.kapp@gmail.com>
2016-07-14 15:49:26 +0300

    Added 'debug' parameter to external configuration

--- config/environments/production_sample.rb
+++ config/environments/production_sample.rb
@@ -57,7 +57,9 @@ ITee::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  if configFile.key?('debug') && configFile['debug']
+    config.log_level = :debug
+  end
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
