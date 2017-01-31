7b3c076da35448b095f8c025288781ed6e2b4609
Keijo Kapp <keijo.kapp@gmail.com>
2017-01-31 12:36:23 +0000

    Removed Devise 'recoverable' feature from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :ldap_authenticatable, #:registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :username, :email, :password, :password_confirmation, :remember_me, :keypair, :token_expires, :role
