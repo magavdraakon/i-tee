0a95b9d22807f43d9a480908d69b5a7a41fefb66
Keijo Kapp <keijo.kapp@gmail.com>
2017-03-09 17:58:33 +0000

    Removed error in remove_hosts migration

--- db/migrate/20170214120138_remove_hosts.rb
+++ db/migrate/20170214120138_remove_hosts.rb
@@ -2,16 +2,4 @@ class RemoveHosts < ActiveRecord::Migration
   def up
     drop_table :hosts
   end
-  def down
-    create_table :hosts do |t|
-      t.string :name
-      t.string :ip
-      t.text :publickey
-      t.text :privatekey
-      t.integer :ram
-      t.integer :cpu_cores
-      t.integer :hdd
-      t.integer :priority
-      t.timestamps
-  end
 end
