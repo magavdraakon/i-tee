fb2715758a40c47e0e577221ac76aaf70e778240
Keijo Kapp <keijo.kapp@gmail.com>
2017-03-29 07:10:21 +0000

    Added abstract_mysql2_adapter hack again

---
+++ config/initializers/abstract_mysql2_adapter.rb
@@ -0,0 +1,5 @@
+if defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
+	class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+		NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+	end
+end
