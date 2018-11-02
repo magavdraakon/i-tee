8ffdd6d929e48533d26ddd5b37abc4d16385536f
root <root@freddie.rangeforce.com>
2018-11-02 15:00:27 +0000

    Updated install script, added Ubuntu18 support

--- install/usr/local/share/guacamole/initdb.mysql.sql
+++ install/usr/local/share/guacamole/initdb.mysql.sql
@@ -278,30 +278,30 @@ CREATE TABLE `guacamole_connection_history` (
 --
 
 -- Create default user "guacadmin" with password "guacadmin"
---INSERT INTO guacamole_user (username, password_hash, password_salt)
---VALUES ('guacadmin',
---    x'CA458A7D494E3BE824F5E1E175A1556C0F8EEF2C2D7DF3633BEC4A29C4411960',  -- 'guacadmin'
---    x'FE24ADC5E11E2B25288D1704ABE67A79E342ECC26064CE69C5B3177795A82264');
+INSERT INTO guacamole_user (username, password_hash, password_salt)
+VALUES ('guacadmin',
+    x'CA458A7D494E3BE824F5E1E175A1556C0F8EEF2C2D7DF3633BEC4A29C4411960',  -- 'guacadmin'
+    x'FE24ADC5E11E2B25288D1704ABE67A79E342ECC26064CE69C5B3177795A82264');
 
 -- Grant this user all system permissions
---INSERT INTO guacamole_system_permission
---SELECT user_id, permission
---FROM (
---          SELECT 'guacadmin'  AS username, 'CREATE_CONNECTION'       AS permission
---    UNION SELECT 'guacadmin'  AS username, 'CREATE_CONNECTION_GROUP' AS permission
---    UNION SELECT 'guacadmin'  AS username, 'CREATE_USER'             AS permission
---    UNION SELECT 'guacadmin'  AS username, 'ADMINISTER'              AS permission
---) permissions
---JOIN guacamole_user ON permissions.username = guacamole_user.username;
+INSERT INTO guacamole_system_permission
+SELECT user_id, permission
+FROM (
+          SELECT 'guacadmin'  AS username, 'CREATE_CONNECTION'       AS permission
+    UNION SELECT 'guacadmin'  AS username, 'CREATE_CONNECTION_GROUP' AS permission
+    UNION SELECT 'guacadmin'  AS username, 'CREATE_USER'             AS permission
+    UNION SELECT 'guacadmin'  AS username, 'ADMINISTER'              AS permission
+) permissions
+JOIN guacamole_user ON permissions.username = guacamole_user.username;
 
 -- Grant admin permission to read/update/administer self
---INSERT INTO guacamole_user_permission
---SELECT guacamole_user.user_id, affected.user_id, permission
---FROM (
---          SELECT 'guacadmin' AS username, 'guacadmin' AS affected_username, 'READ'       AS permission
---    UNION SELECT 'guacadmin' AS username, 'guacadmin' AS affected_username, 'UPDATE'     AS permission
---    UNION SELECT 'guacadmin' AS username, 'guacadmin' AS affected_username, 'ADMINISTER' AS permission
---) permissions
---JOIN guacamole_user          ON permissions.username = guacamole_user.username
---JOIN guacamole_user affected ON permissions.affected_username = affected.username;
+INSERT INTO guacamole_user_permission
+SELECT guacamole_user.user_id, affected.user_id, permission
+FROM (
+          SELECT 'guacadmin' AS username, 'guacadmin' AS affected_username, 'READ'       AS permission
+    UNION SELECT 'guacadmin' AS username, 'guacadmin' AS affected_username, 'UPDATE'     AS permission
+    UNION SELECT 'guacadmin' AS username, 'guacadmin' AS affected_username, 'ADMINISTER' AS permission
+) permissions
+JOIN guacamole_user          ON permissions.username = guacamole_user.username
+JOIN guacamole_user affected ON permissions.affected_username = affected.username;
 
