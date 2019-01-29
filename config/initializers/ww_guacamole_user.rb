882261c492a3823881df3273cef6b3414f8b5ca3
Tiia Tänav <tiiatanav@hotmail.com>
2019-01-29 14:03:50 +0200

    added initializer to create guacamole admin user based on conf to reserve the username

---
+++ config/initializers/ww_guacamole_user.rb
@@ -0,0 +1,10 @@
+# check if user with the username in guacamole config is set
+# if it is not set create it to avoid username stealing
+
+unless user = User.where(username: Rails.application.config.guacamole2['username'] ).first
+	Rails.logger.debug "CREATE User for guacamole-proxy admin"
+	User.create!(username: Rails.application.config.guacamole2['username'], password: Rails.application.config.guacamole2['password'], email: "#{Rails.application.config.guacamole2['username']}@host.local", name:"guacamole admin")
+else
+	Rails.logger.debug "UPDATE User for guacamole-proxy admin"
+	user.update!(username: Rails.application.config.guacamole2['username'], password: Rails.application.config.guacamole2['password'], email: "#{Rails.application.config.guacamole2['username']}@host.local", name:"guacamole admin")
+end
